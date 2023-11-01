f77bfbe00a639c9a492b2acec6202b6997424858
trinigelos <trinidadgelos7@gmail.com>
2023-11-01 14:52:55 +0100

    update Testimonials gallery

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import "./Testimonials.css"
 import TinySlider from "tiny-slider-react";
 import 'tiny-slider/dist/tiny-slider.css';
-import image1 from "../../assets/image1.png";
-import image2 from "../../assets/image2.png";
-import image3 from "../../assets/image3.png";
-import image4 from "../../assets/image4.png";
-import image5 from "../../assets/image5.png";
-import image6 from "../../assets/image6.png";
+import image1 from "../../assets/image1.jpg";
+import image2 from "../../assets/image2.jpg";
+import image3 from "../../assets/image3.jpg";
+import image4 from "../../assets/image4.jpg";
+import image5 from "../../assets/image5.jpg";
+import image6 from "../../assets/image6.jpg";
 
 const Testimonials = () => {
 const imgs = [image1, image2, image3, image4, image5, image6];
@@ -19,7 +19,7 @@ loop: true,
 
 items: 1,
 autoplay: true,
-dots: true,
+dots: false,
 autoplayTimeout: 2000,
 smartSpeed: 450,
 controls: false,
@@ -34,14 +34,14 @@ return (
         <TinySlider settings={settings}>
             {imgs.map((image, index) => (
             <div className="m" key={index}>
-                <img className="tns-item main-image" src={image} alt={`Img ${index}`} />
+                <img className="main-image" src={image} alt={`Img ${index}`} />
             </div>
             ))}
         </TinySlider>
         </div>
         
     <div className='right'>
-        <h1 className='stroke-text'>CAMBIOS </h1>
+        <h1 className='stroke-text'>RESULTADOS</h1>
     </div>
 </div>
 );
