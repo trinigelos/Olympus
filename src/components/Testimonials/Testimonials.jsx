34d8dea1c0dd298c756f228ad8a941e8f5f15499
trinigelos <trinidadgelos7@gmail.com>
2023-11-01 18:19:01 +0100

    Updates testimonials images and layout

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./Testimonials.css"
-import TinySlider from "tiny-slider-react";
-import 'tiny-slider/dist/tiny-slider.css';
 import image1 from "../../assets/image1.jpg";
 import image2 from "../../assets/image2.jpg";
 import image3 from "../../assets/image3.jpg";
@@ -10,36 +8,30 @@ import image5 from "../../assets/image5.jpg";
 import image6 from "../../assets/image6.jpg";
 
 const Testimonials = () => {
-const imgs = [image1, image2, image3, image4, image5, image6];
 
-const settings = {
-loop: true,
-    center: true,
-    mode: "gallery",
+const images = [image1, image2, image3, image4, image5, image6];
 
-items: 1,
-autoplay: true,
-dots: false,
-autoplayTimeout: 2000,
-smartSpeed: 450,
-controls: false,
 
-responsive: {},
+const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+useEffect(() => {
+const interval = setInterval(() => {
+setCurrentImageIndex((prevIndex) =>
+prevIndex === images.length - 1 ? 0 : prevIndex + 1
+);
+}, 1000);
+
+return () => {
+clearInterval(interval);
 };
+}, [images, currentImageIndex]);
 
 return (
 <div className='Testimonials'>
 
     <div className='left'>
-        <TinySlider settings={settings}>
-            {imgs.map((image, index) => (
-            <div className="m" key={index}>
-                <img className="main-image" src={image} alt={`Img ${index}`} />
-            </div>
-            ))}
-        </TinySlider>
-        </div>
-        
+       <div className='outer-div'> <img className='main-image' src={images[currentImageIndex]} alt="" /></div>
+    </div>
     <div className='right'>
         <h1 className='stroke-text'>RESULTADOS</h1>
     </div>
